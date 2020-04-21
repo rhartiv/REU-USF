5d98d281f918049819ee49d64c2b122a98127705
rhartiv <rhartiv@users.noreply.github.com>
2020-04-21 05:37:56 -0400

    Update Grover.py

--- q#/MicrosoftQuickstarts/Grover/Grover.py
+++ q#/MicrosoftQuickstarts/Grover/Grover.py
@@ -1,6 +1,8 @@
 import qsharp
 from Microsoft.Quantum.Samples.SimpleGrover import SearchForMarkedInput
+from Microsoft.Quantum.Samples.SimpleGrover import NIterations
+
 
 n_qubits = 15
-result = SearchForMarkedInput.simulate(nQubits=n_qubits)
+result = [SearchForMarkedInput.simulate(nQubits=n_qubits),NIterations.simulate(nQubits=n_qubits)]
 print(result)
